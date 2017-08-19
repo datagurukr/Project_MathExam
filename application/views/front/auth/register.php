071399f70a7f7788137328cd4ced88609446b96e
DaeunBang <bdw2711@gmail.com>
2017-08-20 00:02:04 +0900

    회원가입

--- application/views/front/auth/register.php
+++ application/views/front/auth/register.php
@@ -62,7 +62,21 @@
                             </div>
                             <div class="row">
                                 <div class="col s12">
-                                    <button class="waves-effect waves-light btn">회원가입</button>
+																	  <!-- Modal Trigger -->
+																		<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+
+																		<!-- Modal Structure -->
+																		<div id="modal1" class="modal">
+																			<div class="modal-content">
+																				<h4>Modal Header</h4>
+																				<p>A bunch of text</p>
+																			</div>
+																			<div class="modal-footer">
+																				<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+																			<button type="submit" class="waves-effect waves-light btn">회원가입</button>
+
+																			</div>
+																		</div>
                                 </div>
                             </div>
                         </div>
