f29a379198a31e76505195232b720fac66409527
DaeunBang <bdw2711@gmail.com>
2017-08-21 04:36:54 +0900

    .

--- application/views/front/body.php
+++ application/views/front/body.php
@@ -131,7 +131,7 @@
         });
         $('.datepicker').pickadate({
             formatSubmit: 'yyyy-mm-dd',
-            selectMonths: true, // Creates a dropdown to control month
+            selectMonths: false, // Creates a dropdown to control month
             selectYears: 15, // Creates a dropdown of 15 years to control year,
             today: 'Today',
             clear: 'Clear',
