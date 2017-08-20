8568264ba3aa46097269c3591fd7cb460a6d4c8e
datagurukr <developer@dataguru.kr>
2017-08-20 23:02:15 +0900

    수

--- application/views/front/body.php
+++ application/views/front/body.php
@@ -116,7 +116,6 @@
         </footer>
     </body>
     <script>
-<<<<<<< Updated upstream
         $(document).ready(function(){
             // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
             $('.modal').modal();
@@ -135,19 +134,8 @@
             close: 'Ok',
             closeOnSelect: false // Close upon selecting a date,
         });
-        
-=======
-    $(document).ready(function(){
-        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-        $('.modal').modal();
-    });
-
-    $(document).ready(function() {
-        $('select').material_select();
-    });
     </script>    
     <script>
->>>>>>> Stashed changes
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
