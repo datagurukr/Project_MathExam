c5c83fa2d54ab113113de1cbf42ef167a9767421
DaeunBang <bdw2711@gmail.com>
2017-08-20 19:53:08 +0900

    문제등록

--- application/views/admin/question/edit.php
+++ application/views/admin/question/edit.php
@@ -146,7 +146,21 @@ if ( $response['status'] == 200 ) {
             </div>
             <div class="row">
                 <div class="input-field col s6">
-                    <button type="submit" class="waves-effect waves-light btn right">적용</a>
+                    <!-- Modal Trigger -->
+                    <a class="waves-effect waves-light btn modal-trigger right" href="#modal1">적용</a>
+
+                    <!-- Modal Structure -->
+                    <div id="modal1" class="modal">
+                        <div class="modal-content">
+                            <h4>수정 검토</h4>
+                            <p>문제 정보를 변경하시겠습니까?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="modal-close waves-effect waves-red btn-flat">최소</button>
+                            <button type="submit" class="modal-action modal-close waves-effect waves-green btn-flat ">승인</button>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="input-field col s6">
                     <?
@@ -155,7 +169,7 @@ if ( $response['status'] == 200 ) {
                         $referer = $_GET['referer'];
                     };
                     ?>
-                    <button type="button" class="waves-effect waves-light btn left" onclick="location.replace('<? echo $referer; ?>');">취소</a>
+                    <button type="button" class="waves-effect waves-light btn left" onclick="location.replace('<? echo $referer; ?>');">취소</button>
                     
                 </div>
             </div>
